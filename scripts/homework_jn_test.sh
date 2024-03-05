48da10d39a8763228f07b0b84df00355152304d8
TomBerton <trberton@gmail.com>
2024-03-04 18:29:48 -0600

    Adds bash scripts and modifies jn yaml

---
+++ scripts/homework_jn_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Iterate over changed files
+for file in $1; do
+    # Check for Python solution files
+    if [[ $file == *solution.ipynb ]]; then
+        echo "Testing $file"
+        gh_runner_path="/home/runner/work/GitHub-Actions-Playground/GitHub-Actions-Playground/"
+        relative_path="${file#$gh_runner_path}"
+        directory_path="$(dirname "$relative_path")"
+        echo "Directory path: $directory_path"
+
+        # Change directory
+        cd $directory_path
+
+        # Run the Python test script
+        python ../../../../scripts/jn_test.py
+
+        # Navigate back to the original directory
+        cd ../../../../
+    fi
+done
