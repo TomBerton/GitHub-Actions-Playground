a2c30c5ac36400bf430350e419fdb7ea55857ecb
TomBerton <trberton@gmail.com>
2024-04-23 15:27:37 -0500

    Testing-7

---
+++ src/04-Functions/Activities/02-Prac_Local_Global/Unsolved/unsolved.py
@@ -0,0 +1,11 @@
+def convert_minutes_to_hours():
+    """ This function takes in the time duration in minutes and returns the time duration in hours."""
+    # Convert minutes to hours.
+
+    # Return the time duration in hours.
+
+
+if __name__ == "__main__":
+    # Prompt the user to enter the time duration in minutes.
+
+    # Call the function and store the result in a variable.
